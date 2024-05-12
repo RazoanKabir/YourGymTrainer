11518e8df17f414482870749cb343dc1f8b7c3d2
Md. Razoan Kabir <kabirrazoan@gmail.com>
2024-05-12 16:11:17 +0600

    Food DAO Added

---
+++ app/src/main/java/com/example/yourgymtrainer/FoodDao.kt
@@ -0,0 +1,31 @@
+package com.example.yourgymtrainer
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface FoodDao {
+    @Upsert
+    suspend fun addFood()
+
+    @Delete
+    suspend fun deleteFood()
+
+    @Query("SELECT * FROM food ORDER BY foodName ASC")
+    suspend fun getAllFoodByAlphabet(): Flow<List<Food>>
+
+    @Query("SELECT * FROM food ORDER BY proteinAmount DESC")
+    suspend fun getFoodByProtein(): Flow<List<Food>>
+
+    @Query("SELECT * FROM food ORDER BY fatAmount DESC")
+    suspend fun getFoodByFat(): Flow<List<Food>>
+
+    @Query("SELECT * FROM food ORDER BY caloriesAmount DESC")
+    suspend fun getFoodByCalorie(): Flow<List<Food>>
+
+    @Query("SELECT * FROM food ORDER BY carbsAmount DESC")
+    suspend fun getFoodByCarb(): Flow<List<Food>>
+}
