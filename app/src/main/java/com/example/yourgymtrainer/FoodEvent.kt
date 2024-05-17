705ee43550be7a8ed2666245c447b5aae1afcbfe
Md. Razoan Kabir <kabirrazoan@gmail.com>
2024-05-17 23:12:59 +0600

    Food Event class added to control user events

---
+++ app/src/main/java/com/example/yourgymtrainer/FoodEvent.kt
@@ -0,0 +1,19 @@
+package com.example.yourgymtrainer
+
+sealed interface FoodEvent {
+    object SaveNewFood: FoodEvent
+    data class SetSearchTerm(val searchTerm: String): FoodEvent
+    data class SetFoodName(val foodName: String): FoodEvent
+    data class SetFoodProtein(val foodProtein: Int): FoodEvent
+    data class SetFoodFat(val foodFat: Int): FoodEvent
+    data class SetFoodCalories(val foodCalories: Int): FoodEvent
+    data class SetFoodCarbs(val foodCarbs: Int): FoodEvent
+    data class SortFoodsByAlphabetical(val sortType: FoodSortType): FoodEvent
+    data class SortFoodsByProtein(val sortType: FoodSortType): FoodEvent
+    data class SortFoodsByFat(val sortType: FoodSortType): FoodEvent
+    data class SortFoodsByCalories(val sortType: FoodSortType): FoodEvent
+    data class SortFoodsByCarbs(val sortType: FoodSortType): FoodEvent
+    data class DeleteFood(val food: Food): FoodEvent
+    object ShowFoodAddDialog: FoodEvent
+    object HideFoodAddDialog: FoodEvent
+}
